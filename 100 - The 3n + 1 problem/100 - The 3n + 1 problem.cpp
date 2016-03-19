4b048cae6d3004a6a8f92fe173f00e5eed030818
jhn <jhn@kth.se>
2016-03-19 19:28:13 +0100

    first commit

---
+++ 100 - The 3n + 1 problem/100 - The 3n + 1 problem.cpp
@@ -0,0 +1,15 @@
+//
+//  100 - The 3n + 1 problem.cpp
+//  uva onlinejudge
+//
+//  Created by Johannes Hörnfeldt Nordström on 2016-03-19.
+//  Copyright (c) 2016 Johannes Nordström. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
