18ac078afbb8b289a8c5997570d9d7478a6c5e0d
jhn <jhn@kth.se>
2016-03-19 21:56:40 +0100

    Done

--- 100 - The 3n + 1 problem/100 - The 3n + 1 problem.cpp
+++ 100 - The 3n + 1 problem/100 - The 3n + 1 problem.cpp
@@ -1,15 +1,48 @@
-//
-//  100 - The 3n + 1 problem.cpp
-//  uva onlinejudge
-//
-//  Created by Johannes Hörnfeldt Nordström on 2016-03-19.
-//  Copyright (c) 2016 Johannes Nordström. All rights reserved.
-//
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+
+int main(){
+    int n, i, j, input1, input2;
+    
+    int maxCycles = INT_MIN;
+    int cycles;
+    
+    //input n
+    while(cin >> i >> j){
+        maxCycles = 0;
+        input1 = i;
+        input2 = j;
+        
+       while (i<=j) {
+    
+           n = i;
+           
+           cycles = 1;
+           
+           //algoritm
+            while (n != 1) {
+                //if n is odd then n <- 3n + 1
+                if (n % 2!=0) {
+                    
+                    n = (3 * n) + 1;
+                    //std::cout << "jämt" << std::endl;
+                }
+                else n = n/2;
+                
+                
+                cycles++;
+            }
+           
+           if (cycles > maxCycles) maxCycles = cycles;
+           
+           i++;
+           
+       }
+        cout << input1 << " " << input2 << " " << maxCycles <<  endl;
+    }
+    
     return 0;
-}
+  }
